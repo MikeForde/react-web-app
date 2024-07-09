d58674e459d6e96407edb1c3936574ea72de82f1
MikeForde <mikeforde@ymail.com>
2024-07-09 18:59:50 +0000

    Attempt6

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,39 @@
+// const webpack = require('webpack');
+
+// module.exports = function override(config, env) {
+//   // Add custom configuration to override crypto createHash function
+//   config.plugins.push(
+//     new webpack.ProvidePlugin({
+//       crypto: 'crypto',
+//     })
+//   );
+
+//   // Add custom code to override createHash method
+//   const crypto = require('crypto');
+//   const crypto_orig_createHash = crypto.createHash;
+//   crypto.createHash = (algorithm) =>
+//     crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
+//   return config;
+// };
+
+// Apply the override early
+const crypto = require('crypto');
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = (algorithm) =>
+  crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  // Add custom configuration to override crypto createHash function
+  // Provide the crypto module
   config.plugins.push(
     new webpack.ProvidePlugin({
       crypto: 'crypto',
     })
   );
 
-  // Add custom code to override createHash method
-  const crypto = require('crypto');
-  const crypto_orig_createHash = crypto.createHash;
-  crypto.createHash = (algorithm) =>
-    crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
-
   return config;
 };
+
 
