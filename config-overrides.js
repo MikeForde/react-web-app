da54081ea27f6608eafcd4d99d97246cb87b6a7a
MikeForde <mikeforde@ymail.com>
2024-07-09 18:21:12 +0000

    attempt5

--- config-overrides.js
+++ config-overrides.js
@@ -1,25 +1,19 @@
-const path = require('path');
-const fs = require('fs');
 const webpack = require('webpack');
 
-// Load the patched version of createEnvironmentHash
-const createEnvironmentHashPath = path.resolve(__dirname, 'patch-createEnvironmentHash.js');
-const createEnvironmentHash = require(createEnvironmentHashPath);
-
 module.exports = function override(config, env) {
-  // Replace the createEnvironmentHash function in the webpack configuration
-  const createEnvironmentHashModulePath = 'react-scripts/config/webpack/persistentCache/createEnvironmentHash.js';
-  const fullPathToCreateEnvironmentHash = require.resolve(createEnvironmentHashModulePath);
-  
-  // Override the file with the patched version
-  fs.writeFileSync(fullPathToCreateEnvironmentHash, `module.exports = ${createEnvironmentHash.toString()}`);
-
-  // Ensure the crypto module is available
+  // Add custom configuration to override crypto createHash function
   config.plugins.push(
     new webpack.ProvidePlugin({
       crypto: 'crypto',
     })
   );
 
+  // Add custom code to override createHash method
+  const crypto = require('crypto');
+  const crypto_orig_createHash = crypto.createHash;
+  crypto.createHash = (algorithm) =>
+    crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
   return config;
 };
+
