e3277b005a4d59a2edc568823e70a172d1192cbd
MikeForde <mikeforde@ymail.com>
2024-07-09 15:28:05 +0000

    rewired added

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  // Add custom configuration to override crypto createHash function
+  config.plugins.push(
+    new webpack.ProvidePlugin({
+      crypto: 'crypto',
+    })
+  );
+
+  // Add custom code to override createHash method
+  const crypto = require('crypto');
+  const crypto_orig_createHash = crypto.createHash;
+  crypto.createHash = (algorithm) =>
+    crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
+  return config;
+};
