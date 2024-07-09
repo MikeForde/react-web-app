bd869010cf0794ae1841704c598a706e0c0baab1
MikeForde <mikeforde@ymail.com>
2024-07-09 15:41:04 +0000

    noMD4

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,30 @@
+const crypto = require('crypto');
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  // Add custom configuration to override crypto createHash function
+  // Ensure crypto module's createHash does not use MD4
+  const crypto_orig_createHash = crypto.createHash;
+  crypto.createHash = (algorithm) =>
+    crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
+
+  // Modify Webpack persistent cache configuration if it exists
+  if (config.cache && config.cache.type === 'filesystem') {
+    config.cache.buildDependencies = {
+      config: [__filename], // This is a workaround to invalidate the cache
+    };
+
+    config.cache.version = crypto
+      .createHash('sha256')
+      .update(config.cache.version || '')
+      .digest('hex');
+  }
+
+  // Add ProvidePlugin to ensure crypto is available
   config.plugins.push(
     new webpack.ProvidePlugin({
       crypto: 'crypto',
     })
   );
-
-  // Add custom code to override createHash method
-  const crypto = require('crypto');
-  const crypto_orig_createHash = crypto.createHash;
-  crypto.createHash = (algorithm) =>
-    crypto_orig_createHash(algorithm === 'md4' ? 'sha256' : algorithm);
 
   return config;
 };
